47edc75bcee3a9d1f9ef2d2aa1d4eab96eeea808
Fernando Mendes <fernando@mendes.codes>
2019-11-06 15:10:39 +0000

    v0.1.3

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HTTPStream.MixProject do
   def project do
     [
       app: :http_stream,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
